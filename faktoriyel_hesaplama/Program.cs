f0f5c808f8504320aa89f4b5b5a7c07422feaf01
mhammedyildirim <61433710+mhammedyildirim@users.noreply.github.com>
2020-03-10 00:12:52 +0300

    Initial commit

---
+++ faktoriyel_hesaplama/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace faktoriyel_hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Faktoriyeli hesaplancak sayiyi giriniz:  ");
+            int sayi = (Convert.ToInt32(Console.ReadLine()));
+            int faktoriyel = 1;
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel = faktoriyel * i;
+            }
+            Console.Write("Faktoriyel : {0} ", faktoriyel);
+
+            Console.ReadKey();
+        }
+    }
+}
